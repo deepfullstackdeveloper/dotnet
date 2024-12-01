0c1f01f9cf3d95b607a56ab0ad96a136e02a43fe
deep <deep>
2024-12-01 20:26:23 +0530

    Added validation at server side.

--- BlogApi/Models/BlogModel.cs
+++ BlogApi/Models/BlogModel.cs
@@ -1,9 +1,13 @@
-﻿namespace BlogApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Models
 {
     public class BlogModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Text is required.")]
         public string Text { get; set; } = string.Empty;
         public DateTime DateCreated { get; set; } = DateTime.Now;
     }
